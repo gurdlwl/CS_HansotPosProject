38e198e674d83939d7f7f381381e13a0183ebf2a
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2018-12-17 16:43:27 +0900

    update

--- hansot_pos/Payment.cs
+++ hansot_pos/Payment.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace hansot_pos
@@ -34,7 +28,6 @@ namespace hansot_pos
             }
             if(CardRadioBtn.Checked == true)
             {
-                Program.
                 return true;
             }
 
